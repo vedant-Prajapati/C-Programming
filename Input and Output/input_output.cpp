e917aa6fab62421246df2cde5896afebc799b726
Code_With_Vedant <127724117+vedant-Prajapati@users.noreply.github.com>
2025-03-16 22:53:28 +0530

    Day 2 :- complete DataTypes, Input & Output, And Operators(arithmetic, increment_decrement, relational) In C++

---
+++ Input and Output/input_output.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age; 
+    cout<<"Enter your age:";
+    cin>>age;
+    cout<<"Your age is:"<<age<<endl;
+
+    // double price; 
+    // cout<<"Enter your price:";
+    // cin>>price;
+    // cout<<"You entered price = "<<age<<endl;
+
+    
+   
+    return 0;
+}
